e9fa77f86ad6912d88f8dc2aadbe2e2b84a358df
daidai21 <728634974@qq.com>
2019-07-08 18:42:09 +0800

    test CI

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="leetcode",
+    packages=find_packages(),
+    description="leetcode.",
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
