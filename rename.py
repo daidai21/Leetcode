e3b6a9def392e197583aca6a32404b3e0da35bd6
daidai21 <728634974@qq.com>
2019-07-09 18:36:46 +0800

    add statistics all code lines to rename.py

--- rename.py
+++ rename.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import os
 import copy
 
@@ -18,8 +19,29 @@ def rename(dir_path):
         os.rename(file_path, new_file_path)
 
 
+def see_all_code_lines(dir_path):
+    '''
+    see all code lines in dir_path
+    '''
+    print(dir_path)
+    file_list = os.listdir(dir_path)
+
+    all_lines = 0
+    for file_name in file_list:
+        file_path = os.path.join(dir_path, file_name)
+        output = os.popen("wc -l " + file_path)
+        # print(output.read().split(' '))
+        try:
+            all_lines += int(output.read().split(' ')[0])
+        except Exception as err:
+            print("Error: ", file_path)
+    print("all_lines: ", all_lines)
+
+
 if __name__ == "__main__":
-    # rename Python file ()
     rename("./Python3.x")
-    # rename Python file ()
     rename("./C++")
+
+    see_all_code_lines("./Python3.x/")
+    see_all_code_lines("./C++/")
+
