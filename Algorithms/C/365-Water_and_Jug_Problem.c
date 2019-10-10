d3d04a009b94462ec383fda1e5904d43075a8f31
daidai21 <daidai4269@aliyun.com>
2019-10-10 15:36:14 +0800

    19.10.10

---
+++ Algorithms/C/365-Water_and_Jug_Problem.c
@@ -0,0 +1,14 @@
+// Runtime: 0 ms, faster than 100.00% of C online submissions for Water and Jug Problem.
+// Memory Usage: 6.7 MB, less than 100.00% of C online submissions for Water and Jug Problem.
+bool canMeasureWater(int x, int y, int z){
+    return z == 0 || (x + y >= z && z % gcd(x, y) == 0);
+}
+
+int gcd(int x, int y) {
+    return y == 0 ? x : gcd(y, x % y);
+}
+
+
+/*
+    gcd(greatest common divisor)
+*/
