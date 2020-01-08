708fa65fbe8d0e9654cb29bfc4684641d5dd5091
daidai21 <daidai4269@aliyun.com>
2020-01-08 19:56:56 +0800

    .

---
+++ Algorithms/Python3.x/669. Trim a Binary Search Tree.py
@@ -0,0 +1,22 @@
+# recursion
+# Runtime: 52 ms, faster than 41.56% of Python3 online submissions for Trim a Binary Search Tree.
+# Memory Usage: 16.8 MB, less than 100.00% of Python3 online submissions for Trim a Binary Search Tree.
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def trimBST(self, root: TreeNode, L: int, R: int) -> TreeNode:
+        if not root:
+            return None
+        elif root.val > R:
+            return self.trimBST(root.left, L, R)
+        elif root.val < L:
+            return self.trimBST(root.right, L, R)
+        else:
+            root.left = self.trimBST(root.left, L, R)
+            root.right = self.trimBST(root.right, L, R)
+            return root
