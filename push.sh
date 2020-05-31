2366e67e66aa3b8d6043ba40147c96e159e11c6b
daidai21 <daidai4269@aliyun.com>
2020-05-31 11:57:18 +0800

    .

--- push.sh
+++ push.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 
-python helper.py -r Algorithms/C
-python helper.py -r Algorithms/C++
-python helper.py -r Algorithms/Python3.x
-python helper.py -r Concurrency/C
-python helper.py -r Concurrency/C++
-python helper.py -r Concurrency/Python3.x
+python3 helper.py -r Algorithms/C
+python3 helper.py -r Algorithms/C++
+python3 helper.py -r Algorithms/Python3.x
+python3 helper.py -r Concurrency/C
+python3 helper.py -r Concurrency/C++
+python3 helper.py -r Concurrency/Python3.x
 git checkout test_case.py
 git add .
 git commit -m "."
