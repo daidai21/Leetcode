c667bb81d48a27aa0c0a7c8af5e8b97d8d887ba0
hu.fwh <hu.fwh@alibaba-inc.com>
2020-08-30 23:36:04 +0800

    .

---
+++ Algorithms/C++/1561-Maximum_Number_of_Coins_You_Can_Get.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxCoins(vector<int>& piles) {
+        std::sort(piles.begin(), piles.end());
+        int res = 0;
+        for (int i = piles.size() / 3; i < piles.size(); i += 2) {
+            res += piles[i];
+        }
+        return res;
+    }
+};
