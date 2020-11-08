dd6764009de483a7e272325717881dbfa7e585f4
hu.fwh <hu.fwh@alibaba-inc.com>
2020-11-08 20:51:34 +0800

    .

--- Algorithms/C++/443-String_Compression.cpp
+++ Algorithms/C++/443-String_Compression.cpp
@@ -43,3 +43,28 @@ public:
         return res;
     }
 };
+
+
+// Space: O(1)
+// Time: O(N)
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int anchor = 0,
+            write = 0;
+        for (int read = 0; read < chars.size(); ++read) {
+            if (read + 1 == chars.size() || chars[read + 1] != chars[read]) {
+                chars[write] = chars[anchor];
+                write++;
+                if (read > anchor) {
+                    for (char c : std::to_string(read - anchor + 1)) {
+                        chars[write] = c;
+                        write++;
+                    }
+                }
+                anchor = read + 1;
+            }
+        }
+        return write;
+    }
+};
